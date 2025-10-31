80ea4b2e765289224113de78b294d6a693861e19
rajprabhe <rajprabhe@gmail.com>
2025-10-31 09:15:58 +0530

    Initial commit -project setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://subtle-anemone-57.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://subtle-anemone-57.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
